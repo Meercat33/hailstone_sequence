6ae957907e1900195fb494e4391008f7c68533c4
Meercat33 <75919686+Meercat33@users.noreply.github.com>
2021-06-21 18:53:18 -0400

    Add files via upload

---
+++ hailstone.py
@@ -0,0 +1,13 @@
+userNum = int(input("Enter a number: "))
+def hailstone(num):
+    if num % 2 == 0:
+        num //= 2
+    elif num % 2 == 1:
+        num*=3
+        num+=1
+    return num
+
+while userNum != 1:
+    print(hailstone(userNum))
+    userNum = hailstone(userNum)
+input()
